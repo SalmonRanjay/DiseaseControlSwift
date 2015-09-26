476e978440d60ac2f46e51243706a4b406c5ab53
ranjay <ranjay@clearlyinnovative.com>
2015-09-26 16:28:49 -0400

    Initial Commit

---
+++ DiseaseControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiseaseControl
+//
+//  Created by ranjay on 9/26/15.
+//  Copyright (c) 2015 ranjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
